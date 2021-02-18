3d228c57ce1b120ebbf05901fb88c59d104966a1
Michael Balc <wi19b016@technikum-wien.at>
2021-02-18 22:21:55 +0100

    Abgabe

---
+++ Berechnungen/SpecFlowFeature1.feature
@@ -0,0 +1,18 @@
+﻿Feature: SpecFlowFeature1
+	Simple calculator for adding two numbers
+
+@mytag
+Scenario: Calculate Sinus
+	Given the number PI
+	When the function is Sinus
+	Then the result should be 0
+
+	Scenario: Calculate Cosinus
+	Given the number PI
+	When the function is Cosinus
+	Then the result should be -1
+
+	Scenario: Calculate Tangens
+	Given the number PI
+	When the function is Tangens
+	Then the result should be 0
---
+++ Berechnungen/SpecFlowFeature1Steps.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using TechTalk.SpecFlow;
+using Xunit;
+
+namespace Berechnungen
+{
+    [Binding]
+    public class SpecFlowFeature1Steps
+    {
+        double pi;
+        double result;
+
+        [Given(@"the number PI")]
+        public void GivenTheNumberPI()
+        {
+            pi = (double)Math.PI;
+        }
+        
+        [When(@"the function is Sinus")]
+        public void WhenTheFunctionIsSinus()
+        {
+            result = (double)Math.Sin(pi);
+        }
+        
+        [When(@"the function is Cosinus")]
+        public void WhenTheFunctionIsCosinus()
+        {
+            result = (double)Math.Cos(pi);
+        }
+        
+        [When(@"the function is Tangens")]
+        public void WhenTheFunctionIsTangens()
+        {
+            result = (double)Math.Tan(pi);
+        }
+        
+        [Then(@"the result should be (.*)")]
+        public void ThenTheResultShouldBe(int p0)
+        {
+            double expected = p0;
+            double actual = result;
+            Assert.Equal(expected, actual);
+        }
+    }
+}
